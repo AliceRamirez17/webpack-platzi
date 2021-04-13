659fb4dc77f6caa13462130d816c20c79c53851e
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2021-04-12 22:40:00 -0500

    Adding webpack, babel, and loaders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from "./templates/Template.js";
+import "./styles/main.css";
+import "./styles/vars.styl";
+
+console.log("hola");
 
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = null || document.getElementById("main");
   main.innerHTML = await Template();
 })();
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  // Elemento inicial de la aplicación
+  entry: "./src/index.js",
+  // Hacia donde enviaremos lo que preparará webpack
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
+  },
+  // Extensiones con el que trabajará
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        // Conectar con babel, archivos module o js
+        test: /\.m?js$/,
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        // Configuración del Loader
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+  ],
+};
