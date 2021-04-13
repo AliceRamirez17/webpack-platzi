e18cac490f84000bc5b247d0c9ca05ffed3eb01a
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2021-04-13 11:30:45 -0500

    Adding more options in webpack.config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 const DotEnv = require("dotenv-webpack");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   // Elemento inicial de la aplicación
@@ -16,6 +13,8 @@ module.exports = {
     filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
+  mode: "development",
+  watch: true,
   // Extensiones con el que trabajará
   resolve: {
     extensions: [".js"],
@@ -81,10 +80,5 @@ module.exports = {
       ],
     }),
     new DotEnv(),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-  },
 };
